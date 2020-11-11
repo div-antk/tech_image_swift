eeb959bb55e937eea8c971ea2ee4604fc7fd90ad
div-antk <51283468+owaii@users.noreply.github.com>
2020-11-11 22:31:52 +0900

    finderにファイルを画像コピー

--- TechImage/TechImageApp.swift
+++ TechImage/TechImageApp.swift
@@ -9,9 +9,30 @@ import SwiftUI
 
 @main
 struct TechImageApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,
+                                                                .userDomainMask,
+                                                                true)[0]
+        print(documentsPath)
+        
+        return true
+    }
+    
+    // 必要に応じて処理を追加
+}
+
+struct TechImageApp_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
     }
 }
