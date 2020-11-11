4297ed05676c6e78adb5d35e2e980eebbb78a4f1
div-antk <51283468+owaii@users.noreply.github.com>
2020-11-11 21:15:27 +0900

    Initial Commit

---
+++ TechImage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TechImage
+//
+//  Created by Takuya Ando on 2020/11/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TechImage/TechImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechImageApp.swift
+//  TechImage
+//
+//  Created by Takuya Ando on 2020/11/11.
+//
+
+import SwiftUI
+
+@main
+struct TechImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
