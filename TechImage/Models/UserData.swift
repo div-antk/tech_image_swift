726da0ce2504f05f66837fe1649bb4d33e092d97
div-antk <51283468+owaii@users.noreply.github.com>
2020-11-14 20:11:32 +0900

    userData（画像情報の保持と通知）の設定

--- TechImage/Models/UserData.swift
+++ TechImage/Models/UserData.swift
@@ -5,4 +5,14 @@
 //  Created by Takuya Ando on 2020/11/14.
 //
 
+/*
+ ObservableObject:複数のviewの状態管理をひとまとまりのオブジェクトとして管理する
+ @Published:ObservableObjectの監視するプロパティに指定する
+ final:継承するのを禁止、サブクラスでオーバーライドされるのを禁止することでデータが変更されないようにする
+ */
+
 import Foundation
+
+final class UserData: ObservableObject {
+  @Published var images = imageInfos
+}
